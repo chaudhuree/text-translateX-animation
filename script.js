1d24236f352f753c08c7238385d7d8b4666c54d9
chaudhuree <chaudhuree00@gmail.com>
2023-12-21 22:24:21 +0600

    loader added with animation

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+// loader animation
+var tl = gsap.timeline()
+
+function time(){
+    var a = 0
+    setInterval(function(){
+        a += Math.floor(Math.random()*20)
+        if(a<100){
+            document.querySelector("#loader h1").innerHTML = a+"%"
+        }else{
+            a = 100
+            document.querySelector("#loader h1").innerHTML = a+"%"
+        }
+    },150)
+}
+
+tl.to("#loader h1",{
+    delay:0.5,
+    duration:1,
+    onStart:time()
+})
+tl.to("#loader",{
+    top:"-100vh",
+    delay:0.4,
+    duration:1.5
+})
+
+// content animation
 gsap.to("#page1 h1", {
 	transform: "translateX(-100%)",
   fontWeight: "100",
